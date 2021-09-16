c25270a2a2f581ef39825cdb921fd3ea98d701dc
GustavoWaki <guwteles@gmail.com>
2021-09-16 15:15:00 -0300

    criando o projeto

---
+++ 20124_20137_Hash/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apBucketHash
+{
+    class Pessoa
+    {
+        string chave,
+               nome;
+
+        public override string ToString()
+        {
+            return Chave.Trim() + " " + Nome.Trim();
+        }
+        public Pessoa(string chave, string nome)
+        {
+            Chave = chave;
+            Nome = nome;
+        }
+
+        public Pessoa(string linhaLida)
+        {
+            Chave = linhaLida.Substring(0, 15);
+            Nome = linhaLida.Substring(15);
+        }
+        public string Chave
+        {
+            get => chave;
+            set => chave = value.ToUpper().PadRight(15, ' ').Substring(0, 15);
+        }
+        public string Nome
+        {
+            get => nome;
+            set => nome = value.PadRight(30, ' ').Substring(0, 30);
+        }
+
+        public string ParaArquivo()
+        {
+            return Chave + Nome;
+        }
+    }
+}
